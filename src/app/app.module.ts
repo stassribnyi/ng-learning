99ef1e9ad111c0c9f486ef8320fe81ab10a5caec
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-02-21 23:15:44 +0200

    implemented base funnctionality of sport store decribed in the book

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { StoreModule } from './store/store.module';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/model/product.repository.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { Product } from "./product.model";
+import { StaticDataSource } from "./static.datasource";
+
+@Injectable()
+export class ProductRepository {
+    private products: Product[];
+    private categories: string[] = [];
+
+    constructor(private dataSource: StaticDataSource) {
+        dataSource.getPrpoducts().subscribe(data => {
+            this.products = data;
+            this.categories = data.map(p => p.category)
+                .filter((c, index, array) => array.indexOf(c) == index).sort();
+        });
+    }
+
+    public getProducts(category: string = null): Product[] {
+        return this.products.filter(p => category == null || p.category == category);
+    }
+
+
+    public getProduct(id: number): Product {
+        return this.products.find(p => p.id == id);
+    }
+
+    public getCategories(): string[] {
+        return this.categories;
+    }
+}
